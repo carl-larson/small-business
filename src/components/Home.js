65d20f94dc0ae0ae4a8f1b13b2d5f27f0c615db0
carl-larson <clarson08@gmail.com>
2020-06-18 20:16:14 -0500

    got the main page table filled in and the json populated

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,49 +8,47 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+import businesses from '../businesses.json'
+
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+// function createData(name, hours, address) {
+//   return { name, hours, address };
+// }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+
+// const rows = createRows();
+// function createRows() {  
+//   for(let id = 0; id < businesses.length; id++) {
+//     return createData(businesses[id].name, businesses[id].Hours, businesses[id].Address)
+//   }
+// }
 
 function Home() {
   const classes = useStyles();
-
+  console.log(businesses)
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Hours</TableCell>
+            <TableCell align="right">Address</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
+          {businesses.map((row) => (
+            <TableRow key={row.id}>
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.Name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.Hours}</TableCell>
+              <TableCell align="right">{row.Address}</TableCell>
             </TableRow>
           ))}
         </TableBody>
